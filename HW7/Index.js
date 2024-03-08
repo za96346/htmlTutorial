03e04418666cfe89e3ebe4a5483acbe5a3e9ed7b
siou <za96346@gmail.com>
2024-03-08 14:53:37 +0800

    add hw7

--- HW7/Index.js
+++
@@ -1,7 +0,0 @@
-fetch("./user.json")
-    .then((v) => v?.json())
-    .then((v) => {
-        console.log("RESULT => ", v)
-        const div1 = document.createElement("div")
-        div1.append(v.data.name)
-    })
