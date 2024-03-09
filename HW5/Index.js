8a0c767956502ac882f5a857ad977487ec4345c8
cma3118 <cma3118@gmail.com>
2024-03-09 18:26:08 +0800

    update

--- HW5/Index.js
+++ HW5/Index.js
@@ -2,9 +2,38 @@
  * 1. 實現map
  * map(iteralFunction, iteralList)
 */
+function Mapper(iteralFunction, iteralList){
+    let result = []
+    for (let i = 0; i < iteralList.length; i++){
+        iteralList[i] = iteralFunction(iteralList[i])
+        result = iteralList
+    }
+    return result
+}    
+let mapResult = Mapper(
+    (v, i) => (v + 10),
+    [1, 2, 3, 4, 5]
+)
+console.log(mapResult)
+
 
 
 /**
  * 2. 實現filter
  * filter(iteralFunction, iteralList)
 */
+function Filter(iteralFunction, iteralList){
+    let result = []
+    for (let i = 0; i < iteralList.length; i++){
+        if (iteralFunction(iteralList[i])){
+            result.push(iteralList[i])
+        }
+    }
+    return result
+}
+
+let filterResult = Filter(
+    (v, i) => (v > 3),
+    [1, 2, 3, 4, 5]
+)
+console.log(filterResult)
