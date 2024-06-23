f213dc307c8b5ff119232cdc220b42f5d1b50c69
cma3118 <cma3118@gmail.com>
2024-06-23 11:51:32 +0800

    update

---
+++ HW16/my-app/src/App.jsx
@@ -0,0 +1,15 @@
+import Footer from './components/Footer'
+import Body from './components/Body'
+import Header from './components/Header'
+
+function App() {
+    return (
+        <div>
+            <Header />
+            <Body />
+            <Footer />
+        </div>
+    );
+}
+
+export default App;
---
+++ HW16/my-app/src/components/Body.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Body = () => {
+    return (
+        <>
+            body
+        </>
+    )
+}
+export default Body
